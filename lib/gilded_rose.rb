20a2c29bb25919f24f61e47c99cae301ba317c2d
Alina Goaga <goaga.alina@gmail.com>
2019-02-14 21:50:21 +0000

    Extracted update quality for normal items and tickets

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,46 +1,65 @@
 require 'item'
+require 'sulfuras'
+require 'brie'
+require 'ticket'
+require 'normal'
 
 class GildedRose
   def initialize(items)
     @items = items
   end
 
+  def update_normal(item)
+    item.quality = item.quality - 1
+  end
+
+  def update_ticket(item)
+    if item.sell_in < 11
+      item.quality = item.quality + 1 if item.quality < 50
+    end
+    if item.sell_in < 6
+      item.quality = item.quality + 1 if item.quality < 50
+    end
+    item.quality = 0 if item.sell_in < 0
+  end
+
   def update_quality
     @items.each do |item|
+      # drop the quality by one for normal products (non tickets, brie, sulfuras)
       if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
+        # quality can't be negative so we need to check
         if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
+          update_normal(item) if item.name != 'Sulfuras, Hand of Ragnaros'
         end
+
       else
+        # tickets and brie
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              item.quality = item.quality + 1 if item.quality < 50
-            end
-            if item.sell_in < 6
-              item.quality = item.quality + 1 if item.quality < 50
-            end
+            update_ticket(item)
           end
         end
       end
+
       if item.name != 'Sulfuras, Hand of Ragnaros'
         item.sell_in = item.sell_in - 1
       end
+
+      # once sell_in becomes negative
       if item.sell_in < 0
         if item.name != 'Aged Brie'
           if item.name != 'Backstage passes to a TAFKAL80ETC concert'
             if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
+              # quality degrades twice as fast
+              update_normal(item) if item.name != 'Sulfuras, Hand of Ragnaros'
             end
           else
-            item.quality = item.quality - item.quality
+            # setting 0 for the tickets once the concert has passed
+            update_ticket(item)
           end
         else
+          # this is for aged brie
           item.quality = item.quality + 1 if item.quality < 50
         end
       end
---
+++ lib/sulfuras.rb
@@ -0,0 +1,5 @@
+require 'item'
+
+class Sulfuras < Item
+  def update_sulfuras(item); end
+end
